86fb26f4723af75dbe8ba02bd3dd0692a19ccb97
nugget <nugget>
2006-05-06 19:29:48 +0000

    Updated help text to cover all currently-implemented commands

--- Localization.lua
+++ Localization.lua
@@ -11,5 +11,11 @@ SLACKERDKP_CHAT_COMMAND_USAGE				= {};
 table.insert(SLACKERDKP_CHAT_COMMAND_USAGE, "Usage: /sdkp <command> [arguments]");
 table.insert(SLACKERDKP_CHAT_COMMAND_USAGE, "Commands:");
 table.insert(SLACKERDKP_CHAT_COMMAND_USAGE, " version - Show SDKP Version and build information");
+table.insert(SLACKERDKP_CHAT_COMMAND_USAGE, " clear - Clear logs and events and start over");
+table.insert(SLACKERDKP_CHAT_COMMAND_USAGE, " toggle - Show/Hide main event log window");
+table.insert(SLACKERDKP_CHAT_COMMAND_USAGE, " att <comments> - Take an attendance snapshot");
+table.insert(SLACKERDKP_CHAT_COMMAND_USAGE, " bosskill <comments> - Log boss kill of targeted corpse");
+table.insert(SLACKERDKP_CHAT_COMMAND_USAGE, " ignore <itemname> - Add itemname to list of ignored drops");
+table.insert(SLACKERDKP_CHAT_COMMAND_USAGE, " unignore <itemname> - Remove itemname from list of ignored drops");
 
 SLACKERDKP_UNKNOWN_COMMAND				= "Unknown Slacker DKP command";
